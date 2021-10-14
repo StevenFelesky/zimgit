768bab3435e301ab256bddb12b128b076521756c
steven <stevenfelesky@gmail.com>
2021-10-14 13:36:20 -0400

    third

--- webstore/views.py
+++ webstore/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import SDCard, Genre
+
+def index(request):
+
+    # Generate counts of some of the main objects
+    num_cards = SDCard.objects.all().count()
+    num_genres = Genre.objects.all().count()
+
+    context = {
+        'num_cards': num_cards,
+        'num_genres': num_genres,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
